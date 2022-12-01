b6906bd70753eac69ee148fccc421d16ce31920e
aagrek <113103582+aagrek@users.noreply.github.com>
2022-12-01 13:47:51 +0300

    Update Proect.py

--- Proect.py
+++ Proect.py
@@ -44,15 +44,15 @@ def method_trapecia():
     return 0
 def method_Sipmson(f,a,b):
     ans1=(b-a)/6*(f(a)+4*f((a+b)/2)+f(b))
-    ans2=f(a+(b-a)/2/100)+f(a+(b-a))
+    ans2=f(a)+f(a+(b-a))
     c1=1
-    for i in range(a*100+(b-1),b*100-(b-a),(b-a)):
+    for i in range(a*110000+(b-1),b*110000-(b-a),(b-a)):
         if c1%2==1:
-            ans2+=2*f(a+(b-a)*c1/2/100)
+            ans2+=2*f(a+(b-a)*c1/2/110000)
         else:
-            ans2+=4*f(a+(b-a)*c1/2/100)
+            ans2+=4*f(a+(b-a)*c1/2/110000)
         c1+=1
-    return ans2*(b-a)/6/100,ans1
+    return ans2*(b-a)/6/110000,ans1
         
     return ans1
 def method_Ghaus():
@@ -67,17 +67,17 @@ def method_Monte_Karlo(f, a, b):
     m2 = 0
     for i in range(a, b):
         s += [f(i)]
-    for i in range(((b - a) * math.ceil(max(s)))**2):
+    for i in range(((b - a) * math.ceil(max(s)))**4):
         x = random.uniform(a, b)
         y = random.uniform(0, math.ceil(max(s)))
         m1 += 1
         if f(x) >= y:
             n1 += 1
-    for i in range(((b - a) * abs(math.floor(min(s))))**2):
+    for i in range(((b - a) * abs(math.floor(min(s))))**4):
         x = random.uniform(a, b)
         y = random.uniform(math.floor(min(s)), 0)
         m2 += 1
         if f(x) <= y:
             n2 += 1
 
-    return -(((n1 / m1) * (b - a) * math.ceil(max(s))) + ((n2 / m2) * (b - a) * math.floor(min(s))))
+    return (((n1 / m1) * (b - a) * math.ceil(max(s))) + ((n2 / m2) * (b - a) * math.floor(min(s))))
