12f928d56dd1412577c1540a2036f42221dbf752
molok31 <80163516+molok31@users.noreply.github.com>
2022-12-07 15:01:39 +0300

    Create test_mod.py

---
+++ test_mod.py
@@ -0,0 +1,12 @@
+from Project import method_Ghaus
+pip install pytest
+pytest test_mod.py
+class TestGhaus:
+  N=1000
+  def test_1():
+    for x in range(self.N):
+      assert methog_Ghaus(cos,x,x+10) == ???
+  def test_2():
+    for x in range(-self.N,0):
+      assert method_Ghaus(cos,x,x+10) == ???
+    
