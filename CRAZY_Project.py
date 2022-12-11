ec04db88171be5f97a4f9889851bdb2c92ef7780
molok31 <80163516+molok31@users.noreply.github.com>
2022-12-11 21:36:18 +0300

    Update CRAZY_Project.py

--- CRAZY_Project.py
+++ CRAZY_Project.py
@@ -1,4 +1,4 @@
-from math import sin,cos,tan
+from math import sin,cos,tan,log
 import random
 import re
 import math
@@ -11,6 +11,9 @@ def step(x,a):
     return x ** a
 def exp(a,x):
     return a**x
+def loga(x,a):
+    return log(x,a)
+
 def integ(f, a, b):
     v, err = integrate.quad(f, a, b)
     return v
@@ -21,6 +24,8 @@ def method_rectangles(f, a, b,*arg):
     for i in range (1,n+1):
         if str(f)[10:14]=='step':
             ans=ans + f((a+shag*(i-1) + (b-a)/2/n),arg[0])
+        elif str(f)[10:14]=='loga':
+            ans=ans + f((a+shag*(i-1) + (b-a)/2/n),arg[0])
         elif str(f)[10:13]=='exp':
             ans=ans + f(arg[0],a+shag*(i-1) + (b-a)/2/n)
         else:
@@ -29,6 +34,8 @@ def method_rectangles(f, a, b,*arg):
 def method_trapecia(f, a, b,*arg):
     result=0
     if str(f)=='step':
+        result+= (f(a,arg[0])+f(b,arg[0]))/2
+    if str(f)=='loga':
         result+= (f(a,arg[0])+f(b,arg[0]))/2
     elif str(f)=='exp':
         result+=(f(arg[0],a)+f(arg[0],b))/2
@@ -39,6 +46,8 @@ def method_trapecia(f, a, b,*arg):
     for i in range(1,n):
         if str(f)=='step':
             result=result + f(a+shag*i,arg[0])
+        elif str(f)=='loga':
+            result=result + f(a+shag*i,arg[0])
         elif str(f)=='exp':
             result=result + f(arg[0],a+shag*i)
         result+=f(a+shag*i)
@@ -46,12 +55,20 @@ def method_trapecia(f, a, b,*arg):
 def method_Simson(f, a, b,*arg):
     if arg=='step':
         ans2=f(a,arg[0])+f(b,arg[0])
+    elif arg=='loga':
+        ans2=f(a,arg[0])+f(b,arg[0])
     elif str(f)=='exp':
         ans2=f(arg[0],a)+f(arg[0],b)
     else:
         ans2=f(a)+f(b)
     
     if str(f)=='step':
+        for i in range(1,10000):
+            if i%2==1:
+                ans2+=4*f(a+(b-a)*i/10000,arg[0])
+            else:
+                ans2+=2*f(a+(b-a)*i/10000,arg[0])
+    elif str(f)=='loga':
         for i in range(1,10000):
             if i%2==1:
                 ans2+=4*f(a+(b-a)*i/10000,arg[0])
@@ -125,13 +142,13 @@ def clever_grafik(f,method):
     plt.plot(t, y1, 'r')
     plt.show()
     
-stroka='sin-step5+exp0.5'
+stroka='sin-step5+exp0.5+loga5'
 fun=method_rectangles
-a=-10
-b=3
+a=10
+b=20
 def podshet(st,a,b,f):
     st=st.split('+')
-    ans=0
+    ans=0   
     for i in st:
         k=i.split('-')
         if len(k)==1:
@@ -139,12 +156,16 @@ def podshet(st,a,b,f):
                 ans+=f(step,  a,b,float(k[0][4:]))
             elif k[0][:3]=='exp':
                 ans+=f(exp,a,b,float(k[0][3:]))
+            elif k[0][:4]=='loga':
+                ans+=f(loga,  a,b,float(k[0][4:]))
             else:ans=ans + f(eval(i), a, b)
         else:
             for j in range(len(k)):
                 if j == 0:
                     if k[j][:4]=='step':
                         ans=ans+f(step,  a,b,float(k[j][4:]))
+                    elif k[j][:4]=='loga':
+                        ans=ans+f(loga,  a,b,float(k[j][4:]))
                     elif k[j][:3]=='exp':
                         ans+=f(exp,a,b,float(k[j][3:]))
                     else:
@@ -152,10 +173,11 @@ def podshet(st,a,b,f):
                 else:
                     if k[j][:4]=='step':
                         ans-=f(step,  a,b,float(k[j][4:]))
+                    elif k[j][:4]=='loga':
+                        ans-=f(loga,  a,b,float(k[j][4:]))
                     elif k[j][:3]=='exp':
                         ans-=f(exp,a,b,float(k[j][3:]))
                     else:
                         ans-= f(eval(k[j]),a,b)
     return ans
 print(podshet(stroka,a,b,fun))
-print(clever_grafik(stroka, fun))
