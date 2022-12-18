65de7ddfd32bd4550c4320e5d04f4c842ca3a329
molok31 <80163516+molok31@users.noreply.github.com>
2022-12-18 19:07:03 +0300

    Update test_mod.py

--- test_mod.py
+++ test_mod.py
@@ -1,12 +1,98 @@
-from Project import method_Ghaus
-pip install pytest
-pytest test_mod.py
-class TestGhaus:
-  N=1000
-  def test_1():
-    for x in range(self.N):
-      assert methog_Ghaus(cos,x,x+10) == ???
-  def test_2():
-    for x in range(-self.N,0):
-      assert method_Ghaus(cos,x,x+10) == ???
+from CRAZY_Project import *
+import unittest
+global e 
+e=0.01
+class TestGhaus(unittest.TestCase):
+ 
+  def test_1(self):
+    a=podshet('loga4',2,9,method_Ghaus )
+    b=8.2152298
+    assert a/b*100 > 93 or abs(a-b)<e
+  def test_2(self):
+    a=podshet('cos',1,2,method_Ghaus )
+    b=0.067826
+    assert a/b*100 > 93 or abs(a-b)<e
+  def test_3(self):
+    a=podshet('sin+cos-loga12+exp0.25',1,10,method_Ghaus )
+    b=-5.4702
+    assert (a/b*100 > 92) or (abs(a-b) < e)
+  def test_4(self):
+    a=podshet('loga2+exp5-tan',1,12,method_Ghaus)
+    b=151693123.746
+    assert a/b*100 > 93 or abs(a-b)<e
+class TestRectangles(unittest.TestCase):
+   
+  def test_1(self):
+    a=podshet('loga4',2,9,method_rectangles)
+    b=8.2152298
+    assert a/b*100 > 93 or abs(a-b)<e
+  def test_2(self):
+    a=podshet('cos',1,2,method_rectangles )
+    b=0.067826
+    assert a/b*100 > 93 or abs(a-b)<e
+  def test_3(self):
+    a=podshet('sin+cos-loga12+exp0.25',1,10,method_rectangles )
+    b=-5.4702
+    assert (a/b*100 > 92) or (abs(a-b) < e)
+  def test_4(self):
+    a=podshet('loga2+exp5-tan',1,12,method_rectangles )
+    b=151693123.746
+    assert a/b*100 > 93 or abs(a-b)<e
+class TestMonteKarlo(unittest.TestCase):
+   
+  def test_1(self):
+    a=podshet('loga4',2,9,method_Monte_Karlo)
+    b=8.2152298
+    assert a/b*100 > 93 or abs(a-b)<e
+  def test_2(self):
+    a=podshet('cos',1,2,method_Monte_Karlo )
+    b=0.067826
+    assert a/b*100 > 93 or abs(a-b)<e
+  def test_3(self):
+    a=podshet('sin+cos-loga12+exp0.25',1,10,method_Monte_Karlo )
+    b=-5.4702
+    assert (a/b*100 > 92) or (abs(a-b) < e)
+  def test_4(self):
+    a=podshet('loga2+exp5-tan',1,12,method_Monte_Karlo )
+    b=151693123.746
+    assert a/b*100 > 93 or abs(a-b)<e
+
+class TestSimson(unittest.TestCase):
+ 
+  def test_1(self):
+    a=podshet('loga4',2,9,method_Simson)
+    b=8.2152298
+    assert a/b*100 > 93 or abs(a-b)<e
+  def test_2(self):
+    a=podshet('cos',1,2,method_Simson )
+    b=0.067826
+    assert a/b*100 > 93 or abs(a-b)<e
+  def test_3(self):
+    a=podshet('sin+cos-loga12+exp0.25',1,10,method_Simson )
+    b=-5.4702
+    assert (a/b*100 > 92) or (abs(a-b) < e)
+  def test_4(self):
+    a=podshet('loga2+exp5-tan',1,12,method_Simson )
+    b=151693123.746
+    assert a/b*100 > 93 or abs(a-b)<e
+
+class TestTrapecia(unittest.TestCase):
     
+  def test_1(self):
+    a=podshet('loga4',2,9,method_trapecia)
+    b=8.2152298
+    assert a/b*100 > 93 or abs(a-b)<e
+  def test_2(self):
+    a=podshet('cos',1,2,method_trapecia )
+    b=0.067826
+    assert a/b*100 > 93 or abs(a-b)<e
+  def test_3(self):
+    a=podshet('sin+cos-loga12+exp0.25',1,10,method_trapecia )
+    b=-5.4702
+    assert (a/b*100 > 92) or (abs(a-b) < e)
+  def test_4(self):
+    a=podshet('loga2+exp5-tan',1,12,method_trapecia )
+    b=151693123.746
+    assert a/b*100 > 93 or abs(a-b)<e  
+  
+ 
